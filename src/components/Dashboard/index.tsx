a30c15745e5262176e83adc72ba85b073e4dddb3
Renato Lobo <renatolobo_@hotmail.com>
2021-03-19 16:51:52 -0300

    Componente: Summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Sumamary } from "../Summary";
+import { Container } from "./styles";
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Sumamary />
+    </Container>
+  );
+}
