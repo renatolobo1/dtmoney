e29472e9f1acb7e6311cd3dafa5e1f802bfd04e7
Renato Lobo <renatolobo_@hotmail.com>
2021-03-21 13:52:55 -0300

    Componente: NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import {GlobalStyle} from "./styles/global";
-import {Header} from './components/Header/index';
-import {Dashboard} from './components/Dashboard';
+import { GlobalStyle } from "./styles/global";
+import { Header } from './components/Header/index';
+import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal} from './components/NewTransactionModal/index';
 import { useState } from "react";
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
 
-  function handleOpenNewTransactionModal(){
+  function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
   }
 
-  function handleOCloseNewTransactionModal(){
+  function handleOCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
   return (
     <>
-     <Header openNewTransactionModal={handleOpenNewTransactionModal}/>
-     <Dashboard />
-
-     <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <Header openNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleOCloseNewTransactionModal}
-        >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
-     <GlobalStyle />
+      />
+      <GlobalStyle />
     </>
   );
 }
