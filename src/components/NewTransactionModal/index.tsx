d3676fa0281c85e05cec83e59301c50fceeec082
Renato Lobo <renatolobo_@hotmail.com>
2021-03-27 21:41:17 -0300

    Finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -20,15 +20,20 @@ const {createTransaction} = useContext(TransactionsContext);
   const [category, setCategory]= useState('');
   const [ type, setType] = useState('');
 
-  function handleCreateNewTransaction(event: FormEvent){
+  async function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
     
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     })
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('');
+    onRequestClose();
   }
   
   return (
